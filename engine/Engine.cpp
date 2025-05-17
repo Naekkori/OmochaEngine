b779785ddc2c2fc2c4d11060538d402d573def7d
Naekkori <spinpo12@naver.com>
2025-05-18 08:19:04 +0900

    Revert "외부인수에서 제어할거라 이부분 제거"
    This reverts commit 37db8ddcc1b36e3ab02484849a0b64c46e5ca6ef.

--- engine/Engine.cpp
+++ engine/Engine.cpp
@@ -276,6 +276,7 @@ bool Engine::loadProject(const string &projectFilePath)
             }
             else
             {
+                this->specialConfig.showFPS = false;
                 EngineStdOut("'specialConfig.showFPS' field missing or not boolean. Using default: false", 1);
             }
             if (specialConfigJson.HasMember("maxEntity") && specialConfigJson["maxEntity"].IsNumber())
